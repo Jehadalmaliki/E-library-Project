d5885886d0cef28da2b98e55a91d57cb2b851400
Jehad Almaliki <yourgitemail@example.com>
2022-04-01 23:23:08 +0300

    BOOKS:Insert|show in dashboard

--- E-library_PHP/mvc/app/controllers/booksController.php
+++ E-library_PHP/mvc/app/controllers/booksController.php
@@ -2,10 +2,29 @@
 namespace coding\app\controllers;
 
 use coding\app\models\Books;
+use coding\app\models\AUthor;
+use coding\app\models\Category;
+use coding\app\models\Publisher;
 
 class BooksController extends Controller{
     function newbooks(){
-        $this->view('new_books');
+        $AUthor=new AUthor();
+        $allAUthors=$AUthor->getAll();
+
+        $categories=new Category();
+        $allCategories=$categories->getAll();
+
+        $Publisher=new Publisher();
+        $allPublisher=$Publisher->getAll();
+
+        $data=["authors" => $allAUthors,
+        "categories" =>$allCategories,
+        "publishers" =>$allPublisher
+    ];
+  
+      $this->view('new_books',$data);
+
+     
     }
     function listAll(){
         $books=new Books();
@@ -16,7 +35,51 @@ class BooksController extends Controller{
 
     }
     public function createbooks(){
-       
+        print_r($_POST);
+        print_r($_FILES);
+        $book=new  Books();
+
+        $imageName=$this->uploadFile($_FILES['image']);
+        $book->image=$imageName!=null?$imageName:"default.png";       
+        $book->title=$_POST['title'];
+        $book->price=$_POST['price'];
+        $book->description=$_POST['description'];
+        $book->pages_number=$_POST['pages_number'];
+        $book->quantity=$_POST['quantity'];
+        $book->format=$_POST['format'];
+        $book->category_id=$_POST['categories'];
+        $book->author_id=$_POST['authors'];
+        $book->publisher_id=$_POST['publishers'];
+        $book->created_by=1;
+        $book->is_active=isset($_POST['is_active'])?1:0;
+
+
+        if($book->save())
+        
+        $this->view('feedback',['success'=>'data inserted successful']);
+        else 
+        $this->view('feedback',['danger'=>'can not add data']);
+    }
+    public static function uploadFile(array $imageFile): string
+    {
+        // check images direction
+        if (!is_dir(__DIR__ . '/../../public/images')) {
+            mkdir(__DIR__ . '/../../public/images');
+        }
+
+        if ($imageFile && $imageFile['tmp_name']) {
+            $image = explode('.', $imageFile['name']);
+            $imageExtension = end($image);
+
+            $imageName = uniqid(). "." . $imageExtension;
+            $imagePath =  __DIR__ . '/../../public/images/' . $imageName;
+
+            move_uploaded_file($imageFile['tmp_name'], $imagePath);
+
+            return $imageName;
+        }
+
+        return null;
     }
 
 }
